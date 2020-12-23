cdf9d8e0a1ec83af357d8735feb9790b3b8a19a5
shicham <sass.hicham@gmail.com>
2020-12-23 19:54:22 +0100

    Update CrmErkServerApplication.java

--- src/main/java/com/crm/erk/server/CrmErkServerApplication.java
+++ src/main/java/com/crm/erk/server/CrmErkServerApplication.java
@@ -3,11 +3,11 @@ package com.crm.erk.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 import org.springframework.context.annotation.Bean;
 
-@EnableZuulServer
+@EnableZuulProxy
 @EnableDiscoveryClient
 @SpringBootApplication
 public class CrmErkServerApplication {

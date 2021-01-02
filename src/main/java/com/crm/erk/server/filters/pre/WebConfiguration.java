b99ddd2ee6f21de662212b65416316b50f61bc47
shicham <sass.hicham@gmail.com>
2021-01-02 12:09:27 +0100

    Create WebConfiguration.java

---
+++ src/main/java/com/crm/erk/server/filters/pre/WebConfiguration.java
@@ -0,0 +1,17 @@
+package com.crm.erk.server.filters.pre;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		System.out.println("CorsRegistry " + registry);
+		registry.addMapping("/**").allowedOrigins("*").allowedOrigins("http://51.68.44.179","http://51.68.44.179/","http://51.68.44.179/authenticate","http://51.68.44.179/#/login","51.68.44.179").allowedMethods("GET", "POST", "DELETE",
+				"PUT");
+	}
+
+}
